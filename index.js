749d7d8b9aa50545de661d5e42ce12cccba8d281
Andrew Wang <Andrewruujin@gmail.com>
2022-08-28 11:35:14 -0400

    commit tree 4252cd41cb829d05b8d6b42dbfb7424a1f919bcc

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log('DOM loaded!');
+    document.getElementById('text').textContent = 'This is really cool!';
+    
+  });
+
+console.log(document.getElementById('text').innerText);
+console.log("This console.log() fires when index.js loads - before DOMContentLoaded is triggered");  
